afb5cb9edb2b9c424d4226e8132690be9759e085
hhhh8815 <55180599+hhhh8815@users.noreply.github.com>
2020-11-15 01:07:44 +0800

    完成

---
+++ 2D_Midterm_106051294/Assets/Script/MenuManager.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    public void StartGame()
+    {
+        print("遊戲開始");
+
+        SceneManager.LoadScene("場景");  // 場景管理.載入場景("名稱")
+        //SceneManager.LoadScene(1);      // 場景管理.載入場景(編號)
+    }
+
+    public void QuitGame()
+    {
+        print("離開遊戲");
+        Application.Quit();
+    }
+}
